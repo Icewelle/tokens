8935193d54f28d65a574e5a85d81fc10752d1fe8
Icewelle <corentin.luby@gmail.com>
2024-10-22 16:16:45 +0200

    changer les env à faire, + d'autre truc j'ai oublié

--- src/tokenization/put_env.c
+++ src/tokenization/put_env.c
@@ -6,7 +6,7 @@
 /*   By: cluby <cluby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 06:37:40 by cluby             #+#    #+#             */
-/*   Updated: 2024/10/12 17:22:29 by cluby            ###   ########.fr       */
+/*   Updated: 2024/10/19 11:49:19 by cluby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,42 +29,42 @@ static void replace_env(t_token *tokens, t_envp *menvp)
 	}
 }
 
-static int	ft_count_words(const char *str)
-{
-	int	i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (str[i])
-	{
-		if (str[i] == '$' || str[i] == ' ')
-			count++;
-		i++;
-	}
-	return (count);
-}
-
-static void put_words(char **str, int words)
-{
-	printf("[%p]\n[%d]\n\n", str, words);
-}
-
 static char	**split_env(const char *str)
 {
-	char	**tenvs;
-	int		words;
+	int len = strlen(str);
+    int i = 0, word_count = 0;
+    char **result = (char **)malloc((len + 1) * sizeof(char *));  // Allocate enough memory
 
-	words = ft_count_words(str);
-	if (!words)
-		return (NULL);
-	tenvs = malloc((words + 1) * sizeof(char *));
-	if (!tenvs)
-		return (NULL);
-	tenvs[words] = NULL;
-	printf("[%p]\n", tenvs);
-	put_words(tenvs, words);
-	return (tenvs);
+    while (i < len) {
+        if (str[i] == ' ' || str[i] == '\t') {
+            result[word_count] = (char *)malloc(2 * sizeof(char));  // Space or tab is a single char plus null terminator
+            result[word_count][0] = str[i];
+            result[word_count][1] = '\0';  // Null-terminate the string
+            word_count++;
+            i++;
+            continue;
+        }
+        int start = i;
+        if (str[i] == '$') {
+            start = i++;  // Skip the '$'
+            while (i < len && (str[i] != ' ' && str[i] != '\t' && str[i] != '$')) {
+                i++;
+            }
+        } else {
+            while (i < len && (str[i] != ' ' && str[i] != '\t' && str[i] != '$')) {
+                i++;
+            }
+        }
+        int word_len = i - start;
+        if (word_len > 0) {
+            result[word_count] = (char *)malloc((word_len + 1) * sizeof(char));
+            strncpy(result[word_count], str + start, word_len);
+            result[word_count][word_len] = '\0';  // Null-terminate the string
+            word_count++;
+        }
+    }
+    result[word_count] = NULL;
+    return result;
 }
 
 static void manage_d_quote(t_token *tokens, t_envp *menvp)
